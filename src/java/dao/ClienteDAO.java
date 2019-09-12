a1d203fb9f4826c0181f8911a26198ed89deba25
GentleWinter <Winter.alisson@gmail.com>
2019-09-12 09:16:17 -0300

    instancar DAO

--- src/java/dao/ClienteDAO.java
+++ src/java/dao/ClienteDAO.java
@@ -41,5 +41,15 @@ public class ClienteDAO {
                 }
     return clientes;
     }
+    public static Cliente instanciarCliente(ResultSet rs) throws SQLException{
+        Cliente cliente = new Cliente(rs.getInt("codCliente"),
+                rs.getString("nome"),
+                rs.getString("cpf"),
+                rs.getString("telefone"),
+                null,
+                null);
+        cliente.setIdPrimariaEndereco(rs.getInt("endereco"));
+        return cliente;
+    }
 }
 
