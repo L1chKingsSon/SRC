5b0eed019f49cbb22aa2eb67a59aa6a5536ef01a
GentleWinter <Winter.alisson@gmail.com>
2019-09-17 09:12:06 -0300

    obterCliente

--- src/java/model/Cliente.java
+++ src/java/model/Cliente.java
@@ -14,4 +14,19 @@ public class Cliente extends Pessoa{
     public Cliente(long id, String nome, String cpf, String telefone, Endereco endereco, ContaBanco contaBanco, int idPrimariaEndereco, int idPrimariaContaBanco) {
         super(id, nome, cpf, telefone, endereco, contaBanco, idPrimariaEndereco, idPrimariaContaBanco);
     }
+    public static  Cliente obterCliente(int codCliente) throws ClassNotFoundException, SQLException {
+        Connection conexao = null;
+        Statement comando = null;
+        Cliente cliente = null;
+        try{
+            conexao = BD.getConexao();
+            comando = conexao.createStatement();
+            ResultSet rs = comando.executeQuery( "select * from cliente where codCliente =" + codCliente);
+            rs.first();
+            curso = instanciarCurso(rs);
+        } finally {
+            fecharConexao(conexao, comando);
+        }
+        return cliente;
+    }
 }
